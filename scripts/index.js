a63f0f2480850bee25e549da81b6fde4ec7af308
Ruslan Gichka <gichkaruslan@yandex.ru>
2022-06-14 15:38:07 +0300

    Nice

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.button_close');
+let formElement = document.querySelector('.popup__form')
+let nameInput = document.querySelector('.popup__input_name-first')
+let profInput = document.querySelector('.popup__input_name-two')
+let nameHuman = document.querySelector('.profile__title')
+let profHuman = document.querySelector('.profile__subtitle')
 
 function OpenPopup() {
 	popup.classList.add('popup__oppened');
@@ -10,38 +15,14 @@ function ClosePopup() {
 	popup.classList.remove('popup__oppened');
 }
 
+function formSubmitHandler(evt) {
+	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+	profHuman.textContent = profInput.value;
+	nameHuman.textContent = nameInput.value;
+}
+
 editButton.addEventListener('click', OpenPopup);
 closeButton.addEventListener('click', ClosePopup);
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form')
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__firstname')
-let profInput = document.querySelector('.popup__prof')
-let nameHuman = document.querySelector('.profile__title')
-let profHuman = document.querySelector('.profile__subtitle')
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-	// Так мы можем определить свою логику отправки.
-	// О том, как это делать, расскажем позже.
-
-	nameInput.value;    // Получите значение полей profInput и nameInput из свойства value
-	profInput.value;
-
-	   // Выберите элементы, куда должны быть вставлены значения полей
-	profHuman.textContent = profInput.value;
-	nameHuman.textContent = nameInput.value;
-	return;
-	// Вставьте новые значения с помощью textContent
-}
-
-//console.log(nameInput.value);
-//console.log(profInput.value);
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 formElement.addEventListener('submit', ClosePopup);
