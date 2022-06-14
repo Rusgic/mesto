86c377a9408b7065e33dafa1ceedb851573a0f12
Rusgic <gichkaruslan@yandex.ru>
2022-06-14 16:58:38 +0300

    правка замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const closeButton = document.querySelector('.button_close');
+const closeButton = document.querySelector('.button_type_close');
 let formElement = document.querySelector('.popup__form')
 let nameInput = document.querySelector('.popup__input_name-first')
 let profInput = document.querySelector('.popup__input_name-two')
 let nameHuman = document.querySelector('.profile__title')
 let profHuman = document.querySelector('.profile__subtitle')
 
+
 function OpenPopup() {
 	popup.classList.add('popup__oppened');
+	nameInput.value = nameHuman.textContent;
+	profInput.value = profHuman.textContent;
 }
 
 function ClosePopup() {
@@ -19,10 +22,12 @@ function formSubmitHandler(evt) {
 	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 	profHuman.textContent = profInput.value;
 	nameHuman.textContent = nameInput.value;
+	ClosePopup();
 }
 
 editButton.addEventListener('click', OpenPopup);
 closeButton.addEventListener('click', ClosePopup);
 
-formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', ClosePopup);
+formElement.addEventListener('submit', formSubmitHandler)
+
+
